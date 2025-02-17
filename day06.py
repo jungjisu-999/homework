0a7b19b1c1332b372acac74e16029939385f8935
deepblue <deepblue@inha.ac.kr>
2025-02-17 10:41:32 +0900

    v0.4 day06 recursion

--- day06.py
+++ day06.py
@@ -1,11 +1,10 @@
-def is_even(n) -> bool:
-    """
-    짝수 판정 함수
-    :param n: 판정할 정수
-    :return: 짝수면 True, 홀수면 False
-    """
-    return not n & 1
+def dec_oct(n) -> int:
+    if n == 0:
+        return ""
+    else:
+        return dec_oct(n // 8) + str(n % 8)
 
 
 n = int(input())
-print(is_even(n))
+print(dec_oct(n))
+# print(n, oct(n))
