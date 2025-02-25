d46552c0e9a3ca0bdb35087ed58132db4b2c4cc1
unknown <jisu1008@inha.edu>
2025-02-25 23:13:40 +0900

    v0.2 day12 homework

--- day12.py
+++ day12.py
@@ -1,24 +1,43 @@
 # Assignment
 # v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 다양한 방법을 적용하시오.
 
-import seaborn as sns
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-# from sklearn.neighbors import KNeighborsRegressor  # 적용모델 : K 최근접 이웃 회귀 모델 (주석 처리)
-# from sklearn.model_selection import train_test_split  # 훈련 / 검증 셋트 분할 함수 (주석 처리)
+#from sklearn.impute import SimpleInputer # Annotating the Syketrun Library
 
-# 나이에 따른 생존율 계산
-titanic = sns.load_dataset('titanic')  # 데이터 로딩
-mean_age = titanic['age'].mean()  # 나이 평균값 산출
-titanic_fill_row = titanic.fillna({'age': mean_age})  # 결측치 처리 (평균값으로 채움)
+#Create Example Data
+df = pd.DataFrame(
+    {
+        'A' : [1, 2,  np.nan, 4],
+        'B' : [np.nan, 12, 3, 4],
+        'C' : [1, 2, 3, 4]
+    }
+)
 
-X = titanic_fill_row[['age']]  # 독립 변수 설정
-y = titanic_fill_row[['survived']]  # 종속 변수 설정
+print("data")
+print(df)
 
-# 시각화 (데이터 분포 확인용)
-plt.figure(figsize=(5, 2))
-plt.scatter(X, y, color='blue', label='Data Points')
-plt.title('Titanic Age vs Survived')
-plt.xlabel('Age')
-plt.ylabel('Survived')
+#Filling missing values by arithmetic mean
+A_mean = df['A'].mean()
+B_mean = df['B'].mean()
+df['A'].fillna(A_mean, inplace=True)
+df['B'].fillna(B_mean, inplace=True)
+
+print("\n 결측치 평균값을 채운 후 데이터")
+print(df)
+
+# Visualization (A,B column data distribution)
+plt.figure(figsize=(6, 4))
+
+#Visualize column A
+plt.scatter(range(len(df)), df['A'], color='blue', label='A (Age)')
+
+#Visualize column B
+plt.scatter(range(len(df)), df['B'], color='red', label='B (Other Variable)')
+
+plt.title('A vs B Column Distribution After Filling Missing Values')
+plt.xlabel('Index')
+plt.ylabel('Values')
 plt.legend()
 plt.show()
